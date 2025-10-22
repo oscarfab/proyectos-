d041de766623e88249e2725386d86dee6c3e288d
oscar <kintosoljaep12@gmail.com>
2025-10-22 13:11:33 -0700

    subiendo cambios para la intercepcion delas exepciones

--- crud_con_Oracle/src/main/java/crudconconexionOracle/com/example/crud_con_Oracle/repository/EmpleadoRepository.java
+++ crud_con_Oracle/src/main/java/crudconconexionOracle/com/example/crud_con_Oracle/repository/EmpleadoRepository.java
@@ -4,6 +4,8 @@ import jakarta.persistence.metamodel.SingularAttribute;
 import crudconconexionOracle.com.example.crud_con_Oracle.modelo.Empleado;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
@@ -12,5 +14,6 @@ import java.util.Optional;
 public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
     Optional<Empleado> findById(SingularAttribute<AbstractPersistable, Serializable> id);
     // En EmpleadoRepository.java
-    boolean existsByCorreo_electronico(String correo_electronico);
+    @Query("SELECT COUNT(e) > 0 FROM Empleado e WHERE e.correo_electronico = :correo")
+    boolean existsByCorreo_electronico(@Param("correo") String correo);
 }
