a7ba5218ca347dc537f98cfc69d40c817b36e0c2
oscar <kintosoljaep12@gmail.com>
2025-10-25 12:10:12 -0700

    subiendo cambios

--- crud_con_Oracle/src/test/java/crudconconexionOracle/com/example/crud_con_Oracle/exeption/GlobalExceptionHandlerTest.java
+++ crud_con_Oracle/src/test/java/crudconconexionOracle/com/example/crud_con_Oracle/exeption/GlobalExceptionHandlerTest.java
@@ -25,7 +25,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-@SpringBootTest
+
 @Nested
 @DisplayName("Pruebas del Manejador Global de Excepciones")
 class GlobalExceptionHandlerTest {
